21b0ab13857613f95c36e1cbe6e8c62bbcfb11d6
Amrit Saha <amritsaha607@gmail.com>
2024-07-28 16:35:52 +0530

    allow cross site headers

--- MailServer/settings.py
+++ MailServer/settings.py
@@ -25,9 +25,12 @@ SECRET_KEY = 'django-insecure-s#ez#6bvvcgynix706_blu)r@8w7&iai2_^(%9ndbn5y5io%74
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
-
+ALLOWED_HOSTS = [
+    'localhost'
+]
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'MailServer.urls'
