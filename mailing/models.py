e3bedbf35181c3474db58e37d6d094b1659bff4c
Amrit Saha <amritsaha607@gmail.com>
2024-10-02 10:57:17 +0530

    fix model attribute name

--- mailing/models.py
+++ mailing/models.py
@@ -23,7 +23,7 @@ class User(models.Model):
 
 
 class RawEvent(models.Model):
-    paylaod = models.TextField()
+    payload = models.TextField()
     received_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
